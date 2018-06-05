c95bb0e7c004f448e556d93a2215c23015695613
Marco Velasquez <marcovelasquez90@hotmail.com>
2018-06-04 22:14:01 -0500

    v1.5 actualizacion

--- C_a.js
+++ C_a.js
@@ -7,23 +7,46 @@ var Billetes={
         {"c":100,"v":10}
     ],
     "Euros":[
-        {"c":100,"v":2000},
+        {"c":100,"v":200},
         {"c":100,"v":100},
         {"c":100,"v":50},
         {"c":100,"v":20},
         {"c":100,"v":10}
     ],
     "Soles":[
-        {"c":100,"v":20000},
+        {"c":100,"v":200},
         {"c":100,"v":100},
         {"c":100,"v":50},
         {"c":100,"v":20},
         {"c":100,"v":10}
     ]
 };
-//var T_A = 0; // Total de Efectivo actual
 
-//var resultado="";
+var cant = 0;
+var rest = 0;
+var rB = {
+    "Dolares":[
+        {"c":0,"v":200},
+        {"c":0,"v":100},
+        {"c":0,"v":50},
+        {"c":0,"v":20},
+        {"c":0,"v":10}
+    ],
+    "Euros":[
+        {"c":0,"v":200},
+        {"c":0,"v":100},
+        {"c":0,"v":50},
+        {"c":0,"v":20},
+        {"c":0,"v":10}
+    ],
+    "Soles":[
+        {"c":0,"v":200},
+        {"c":0,"v":100},
+        {"c":0,"v":50},
+        {"c":0,"v":20},
+        {"c":0,"v":10}
+    ]
+};;
 
 var cajero = angular.module('Cajero',[]);
 cajero.controller('contenedor',function($scope){
@@ -32,7 +55,6 @@ cajero.controller('contenedor',function($scope){
 
     $scope.totalActual=function(tp){
         var T_A=0;
-        console.log(tp);
         for(var i in Billetes[tp]){
             T_A = T_A + (Billetes[tp][i].c * Billetes[tp][i].v);  
         }
@@ -40,7 +62,6 @@ cajero.controller('contenedor',function($scope){
     }
 
     $scope.Retirar = function(){
-        console.log($scope.Dinero);
         if($scope.moneda==null || $scope.moneda==""){
             $scope.res="Tipo de Moneda no Seleccionada";
         }
@@ -52,6 +73,29 @@ cajero.controller('contenedor',function($scope){
         }
         else if($scope.Dinero % 1 != 0){
             $scope.res="No se permiten decimales";
+        }
+        else if($scope.totalActual($scope.moneda) < $scope.Dinero){
+            $scope.res="Cajero no Cuenta con la Cantidad Solicitada";
+        }
+        else{
+            for (var B in Billetes[$scope.moneda]) {
+                if(Billetes[$scope.moneda][B].c > 0 ){
+                    cant = Math.floor($scope.Dinero / parseInt(Billetes[$scope.moneda][B].v));
+                    if(cant > Billetes[$scope.moneda][B].c){
+                        rest = cant - Billetes[$scope.moneda][B].c;
+                        cant = cant - rest;
+                    }
+                    rB[$scope.moneda][B].c = rB[$scope.moneda][B].c + cant;
+                    $scope.Dinero = $scope.Dinero - (cant * Billetes[$scope.moneda][B].v);
+                    Billetes[$scope.moneda][B].c = Billetes[$scope.moneda][B].c - cant;
+                    if($scope.Dinero == 0){
+                        if(Billetes[$scope.moneda][B].c <= 0){
+                            delete Billetes[$scope.moneda][B];
+                        }
+                        break;
+                    }
+                }
+            }
         }
     }
 });
